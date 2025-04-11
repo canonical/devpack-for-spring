fc51855f2b5f98a4a3ed341831e0448ea94b3ff1
Vladimir Petko <vladimir.petko@canonical.com>
2025-04-11 18:00:43 +1200

    chore: integrate plugin commands for spring cli (#38)
    * feat: integrate plugin commands for spring cli
    
    - new Spring CLI release v0.10.0-c2
    - added plugin configuration file
    - updated cli command to set path to plugin configuration file
    
    * chore: use bugfix version of spring cli
    
    * chore: bump snap release version to 1.0.0
    
    * chore: bump java-rockcraft-plugins version
    
    * chore: spring-cli v0.10.0-c2.2

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+export SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION=${SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION:-/snap/devpack-for-spring/current/build-plugins.yaml}
 $SNAP/usr/bin/java -jar $SNAP/cli/spring-cli-*.jar $*
