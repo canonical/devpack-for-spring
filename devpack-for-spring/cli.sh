d17831617d6036a4debbd95d7e0ff3fc0f3cbfd9
Vladimir Petko <vladimir.petko@canonical.com>
2025-09-27 09:08:41 +1200

    feat: devpack-for-spring setup command (#114)
    * feat: add setup command to devpack-for-spring
    
    * chore: set metadata fields
    
    * chore: bump version
    
    * chore: rename configuration files
    
    * fix: use cli.sh to set environment variables.
    
    * chore: bump devpack-for-spring-cli version
    
    * doc: add setup command
    
    * fix(doc): fix typo in docker.io install

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-export SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION=${SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION:-/snap/devpack-for-spring/current/build-plugins.yaml}
-$SNAP/usr/bin/java -jar $SNAP/cli/spring-cli-*.jar $*
+#!/bin/bash
+export SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION=${SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION:-/snap/devpack-for-spring/current/plugin-configuration.yaml}
+export SPRING_CLI_SETUP_COMMANDS_CONFIGURATION=${SPRING_CLI_SETUP_COMMANDS_CONFIGURATION:-/snap/devpack-for-spring/current/setup-configuration.yaml}
+$SNAP/cli/devpack-for-spring-cli $*
