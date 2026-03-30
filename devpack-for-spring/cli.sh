df25b5423bf376f97b6b47eb6272d07b5eaf1f68
Vladimir Petko <vladimir.petko@canonical.com>
2026-03-31 10:39:55 +1300

    feat: use aot cache, build from main

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -2,23 +2,17 @@
 set -e
 export SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION=${SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION:-/snap/devpack-for-spring/current/plugin-configuration.yaml}
 export SPRING_CLI_SETUP_COMMANDS_CONFIGURATION=${SPRING_CLI_SETUP_COMMANDS_CONFIGURATION:-/snap/devpack-for-spring/current/setup-configuration.yaml}
-AOT_PATH="$SNAP_USER_DATA/devpack-for-spring-cli.aot"
 
-if [ ! -f "$AOT_PATH" ]; then
-    AOT_FLAG="-XX:AOTCacheOutput=$AOT_PATH"
-else
-    if ! $SNAP/usr/bin/java --enable-native-access=ALL-UNNAMED -XX:AOTCache="$AOT_PATH" -XX:AOTMode=on -version > /dev/null 2>&1; then
-        rm -f "$AOT_PATH"
-        AOT_FLAG="-XX:AOTCacheOutput=$AOT_PATH"
-    else
-        AOT_FLAG="-XX:AOTCache=$AOT_PATH -XX:AOTMode=on"
-    fi
-fi
+NATIVE_ACCESS="--sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED"
+AOT_PATH="$SNAP/cli/devpack-for-spring-cli.aot"
+AOT_FLAG="-XX:AOTCache=$AOT_PATH -XX:AOTMode=on"
+CLASSPATH=$(find $SNAP/cli/BOOT-INF/lib/ -type f -name "*.jar" -print | sort | tr '\n' ':')
 
 $SNAP/usr/bin/java \
     $AOT_FLAG \
     -Xlog:aot=error \
     -Dspring.aot.enabled=true \
     -Dio.netty.noUnsafe=true \
-    --enable-native-access=ALL-UNNAMED \
-    -jar $SNAP/cli/devpack-for-spring-cli.jar $*
+    $NATIVE_ACCESS \
+    -cp $SNAP/cli/devpack-for-spring-cli.jar:$CLASSPATH \
+    org.springframework.cli.DevpackForSpringCliApplication $*
