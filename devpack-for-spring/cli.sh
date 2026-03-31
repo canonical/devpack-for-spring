7a3a079633a294682d85633133164f2a72c3779d
Vladimir Petko <vladimir.petko@canonical.com>
2026-03-31 14:28:22 +1300

    feat: do not override plugin config location

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-export SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION=${SPRING_CLI_BUILD_COMMANDS_PLUGIN_CONFIGURATION:-/snap/devpack-for-spring/current/plugin-configuration.yaml}
 export SPRING_CLI_SETUP_COMMANDS_CONFIGURATION=${SPRING_CLI_SETUP_COMMANDS_CONFIGURATION:-/snap/devpack-for-spring/current/setup-configuration.yaml}
 
 NATIVE_ACCESS="--sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED"
