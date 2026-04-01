ed7c3889af774388c339391434c9c7e3086a416a
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-02 10:28:37 +1300

    feat: repack devpack-for-spring boot jar to be aot compatible

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -5,9 +5,14 @@ export SPRING_CLI_SETUP_COMMANDS_CONFIGURATION=${SPRING_CLI_SETUP_COMMANDS_CONFI
 NATIVE_ACCESS="--sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED"
 AOT_PATH="$SNAP/cli/devpack-for-spring-cli.aot"
 AOT_FLAG="-XX:AOTCache=$AOT_PATH -XX:AOTMode=on"
-CLASSPATH=$(find $SNAP/cli/BOOT-INF/lib/ -type f -name "*.jar" -print | sort | tr '\n' ':')
+CLASSPATH=$(find $SNAP/cli/lib/ -type f -name "*.jar" -print | sort | tr '\n' ':')
+
+if [ -n "$DEVPACK_FOR_SPRING_DEBUG_FLAG" ]; then
+    AOT_FLAG=""
+fi
 
 $SNAP/usr/bin/java \
+    $DEVPACK_FOR_SPRING_DEBUG_FLAG \
     $AOT_FLAG \
     -Xlog:aot=error \
     -Dspring.aot.enabled=true \
