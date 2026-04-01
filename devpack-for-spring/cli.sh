66c7f978875558489d9aa273bddc32a680dcb37b
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-02 11:35:53 +1300

    fix: strip trailing ':' in classpath

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -5,7 +5,7 @@ export SPRING_CLI_SETUP_COMMANDS_CONFIGURATION=${SPRING_CLI_SETUP_COMMANDS_CONFI
 NATIVE_ACCESS="--sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED"
 AOT_PATH="$SNAP/cli/devpack-for-spring-cli.aot"
 AOT_FLAG="-XX:AOTCache=$AOT_PATH -XX:AOTMode=on"
-CLASSPATH=$(find "$SNAP/cli/lib/" -type f -name "*.jar" -print | sort | tr '\n' ':')
+CLASSPATH=$(find "$SNAP/cli/lib/" -type f -name "*.jar" -print | sort | tr '\n' ':' | sed 's/\(.*\):/\1/')
 
 if [ -n "$DEVPACK_FOR_SPRING_DEBUG_FLAG" ]; then
     AOT_FLAG=""
