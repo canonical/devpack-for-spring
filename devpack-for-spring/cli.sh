9a8c2c44a83ed4bb6c97c827a141c01082a89f75
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-02 14:37:22 +1300

    fix: store classpath in a text file

--- devpack-for-spring/cli.sh
+++ devpack-for-spring/cli.sh
@@ -4,17 +4,17 @@ export SPRING_CLI_SETUP_COMMANDS_CONFIGURATION=${SPRING_CLI_SETUP_COMMANDS_CONFI
 
 NATIVE_ACCESS="--sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED"
 AOT_PATH="$SNAP/cli/devpack-for-spring-cli.aot"
-AOT_FLAG="-XX:AOTCache=$AOT_PATH -XX:AOTMode=on"
-CLASSPATH=$(find "$SNAP/cli/lib/" -type f -name "*.jar" -print | sort | tr '\n' ':' | sed 's/\(.*\):/\1/')
+AOT_FLAG="-XX:AOTCache=$AOT_PATH -XX:AOTMode=on -Xlog:aot=error"
+CLASSPATH=$(envsubst < "$SNAP/cli/classpath.txt")
 
 if [ -n "$DEVPACK_FOR_SPRING_DEBUG_FLAG" ]; then
     AOT_FLAG=""
+    set -ex
 fi
 
 "$SNAP/usr/bin/java" \
     $DEVPACK_FOR_SPRING_DEBUG_FLAG \
     $AOT_FLAG \
-    -Xlog:aot=error \
     -Dspring.aot.enabled=true \
     -Dio.netty.noUnsafe=true \
     $NATIVE_ACCESS \
